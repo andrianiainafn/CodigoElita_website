926e56c8b63848b2d96f27596c5594da911ccc2d
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-09-01 16:15:43 +0300

    adding breakpoint definition

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens:{
+          'sm': '320px',         // Mobile (Portrait)
+          'md': '768px',         // Mobile (Landscape)
+          'lg': '1024px',        // Tablet
+          'xl': '1280px',        // Small Desktop
+          '2xl': '1440px',       // Medium Desktop
+          '3xl': '1920px',       // Large Desktop
+      }
+     
     },
   },
   plugins: [],
