82d88dba817b1ef65e962e1b1acd9a13d90e3d3a
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-09-04 21:08:52 +0300

    add key

---
+++ portfolio/src/app/Components/Footer.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <div className='bg-[#131313]'>
+        <div className="flex justify-between items-center">
+            <div className="">
+                <h3 className='text-[#fff] font-semibold'>.Untitled</h3>
+            </div>
+            <div className="">
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+            </div>
+        </div>
+        <div className="">
+            <div className="">
+
+            </div>
+            <div className="">
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
