a9ac5c253e0c5d4abf4e9d9ec5e1b9a79f9e84a6
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-09-05 23:26:29 +0300

    modification ui footer,contact,invitation

--- portfolio/src/app/Components/Footer.tsx
+++ portfolio/src/app/Components/Footer.tsx
@@ -6,9 +6,9 @@ type Props = {}
 
 export default function Footer(props: Props) {
   return (
-    <div className='bg-[#131313] text-[#fff] space-y-4 padding-x padding-y'>
+    <div className='bg-[#131313] text-[#fff] space-y-4 py-12 px-20 footer'>
         <div className="flex justify-between items-start flex-col md:flex-row ">
-            <div className="w-[100%] md:w-[40%]">
+            <div className="w-[100%] md:w-[40%] text-center md:text-start">
                 <h3 className='font-semibold text-[2.5em]'>.Untitled</h3>
             </div>
             <div className="flex justify-between items-start md:w-[45%] w-[100%] mt-5 list space-y-3">
