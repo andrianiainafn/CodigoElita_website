76259588d36fe2a5e5954027d436658f92f3c43d
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-10 18:24:50 +0300

    change team information

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,13 +1,11 @@
 import Contact from '@/app/Components/Contact'
 import Service from '@/app/Components/Service'
-import NavBar from './Components/NavBar/NavBar'
 import Header from './Components/Header'
 import About from './Components/About'
 import Footer from './Components/Footer'
 import Stats from './Components/Stats'
 import Incitation from './Components/Incitation'
 import Members from './Components/Members/Members'
-import Project from './Components/Project/Project'
 
 export default function Home() {
   return (
